c08d5f1df38a2bc5dc138c032139c2b53a3f6ec4
Kabachok <caehka0nakypen@gmail.com>
2026-02-20 16:11:18 +0300

    добавлена нормальная возможность использовать help

--- src/main/java/org/janusvault/cli/InitCommand.java
+++ src/main/java/org/janusvault/cli/InitCommand.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 
 @Command(
         name = "init",
+        mixinStandardHelpOptions = true,
         description = "Создать новое хранилище"
 )
 public class InitCommand implements Runnable {
